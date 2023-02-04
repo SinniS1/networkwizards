6c802acff7e8b000b2a6ebbddf9fa7e2dc316654
SinniS1 <sinniscode@gmail.com>
2023-02-04 15:24:09 +0530

    typing effect tuned & cards added

---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Box from '../Box/Box'
+import "../IntroPage/IntroPage.css"
+import "./Cards.css"
+const Cards = () => {
+    return (
+        <div className='Cards IntroContent'>
+            <div className="heading">Hello</div>
+            <div className="NetWiz">Sachin</div>
+            <div className="para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores quod ex eveniet ratione unde, molestiae amet blanditiis vitae iusto voluptatum harum a, assumenda, magni tempore cum eligendi. Veniam, magnam optio?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, dolorem facilis aliquam quas, maxime distinctio, esse laudantium voluptatum sunt sint modi! Dicta ex obcaecati impedit, nihil quam dolor commodi veniam.</div>
+            <Box />
+        </div>
+
+    )
+}
+
+export default Cards
--- src/components/ContentPage/ContentPage.js
+++ src/components/ContentPage/ContentPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Cards from '../Cards/Cards'
 import IntroPage from '../IntroPage/IntroPage'
 const ContentPage = () => {
   return (
@@ -6,6 +7,13 @@ const ContentPage = () => {
     <div className='ContentPage'>
       <IntroPage />
       {/* div for next cards section */}
+      <div className="CardSection">
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+      </div>
     </div>
 
   )
