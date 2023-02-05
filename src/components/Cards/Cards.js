4e5f4e4bd8e39184dff560bc1986103297e537cc
SinniS1 <sinniscode@gmail.com>
2023-02-05 15:27:45 +0530

    some changes

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Box from '../Box/Box'
+// import Box from '../Box/Box'
 import "../IntroPage/IntroPage.css"
 import "./Cards.css"
 const Cards = () => {
@@ -9,7 +9,6 @@ const Cards = () => {
             <div className="NetWiz">Sachin</div>
             <div className="para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores quod ex eveniet ratione unde, molestiae amet blanditiis vitae iusto voluptatum harum a, assumenda, magni tempore cum eligendi. Veniam, magnam optio?
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, dolorem facilis aliquam quas, maxime distinctio, esse laudantium voluptatum sunt sint modi! Dicta ex obcaecati impedit, nihil quam dolor commodi veniam.</div>
-            <Box />
         </div>
 
     )
